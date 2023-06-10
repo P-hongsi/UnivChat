ea8890402729a53cab8e1a92a2c32ebec4acebce
박홍시 <107135571+P-hongsi@users.noreply.github.com>
2023-06-10 10:51:34 +0000

    수정

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,16 +2,19 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_PROMPT = `다음 대화와 후속 질문이 주어지면 후속 질문을 독립형 질문으로 바꾸십시오.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+const QA_PROMPT = `You are a useful AI chatbot that answers in Korean when asked about the department scores of Pusan ​​National University and Pukyong National University. Use the following pieces of context to answer the question at the end.
+You must speak only in Korean.
+Use as much detail as possible when responding.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+If you don't know the answer, think as much as you can and tell me something similar.
+
 
 {context}
 
@@ -20,7 +23,7 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
+    temperature: 0.2, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
