b13fe1ceb48202ff6f4f845cf0374b933d0a84eb
박홍시 <107135571+P-hongsi@users.noreply.github.com>
2023-06-10 12:52:07 +0000

    수정

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,11 +9,10 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a useful AI chatbot that answers in Korean when asked about the department scores of Pusan ​​National University and Pukyong National University.
+const QA_PROMPT = `You are a useful AI chatbot that answers in Korean when asked about the department scores of Busan ​​National University and Bukyong National University.
 Use the following pieces of context to answer the question at the end.
 You must speak only in Korean.
 Use as much detail as possible when responding.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 If you don't know the answer, think as much as you can and tell me something similar.
 
 
@@ -24,7 +23,7 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0.15, // increase temepreature to get more creative answers
+    temperature: 0.3, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
